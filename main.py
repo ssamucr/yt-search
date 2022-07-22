0b5317ce4651434e60b353159b384e121f23452e
Samuel Castro <ssamucr@gmail.com>
2022-07-21 21:13:11 -0500

    main.py errors fixed

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import re
 import pywhatkit
 
 # ----------------------------------------------------------------------------------------------------
-# This code receives a key word to search, open the default browser and searches the first occurrence
+# This code receives a key word to search, open the default browser and play the first occurrence
 # ----------------------------------------------------------------------------------------------------
 
 # Receives the keyword to search
 search_keyword = re.sub(" ", "+", input("Texto a buscar: "))
 
-# Create an http.client.HTTPResponse object with the results info
+# Create a http.client.HTTPResponse object with the results info
 html = urllib.request.urlopen(f"https://www.youtube.com/results?search_query={search_keyword}")
 
 # Convert the object to a string
 string = html.read().decode()
 
 # print(re.findall(r'/watch\?v=.{11,11}', cadena))
-video_ids = re.findall(r'/watch\?v=(.{11,11})', string)  # Stores a list with the ids of the videos
+video_ids = re.findall(r'/watch\?v=(.{11})', string)  # Stores a list with the ids of the videos
 
 # Eliminates the repeated results
+actual = ""
+cont = 1
 for x in range(len(video_ids)):
     if x == 0:
         actual = video_ids[x]
